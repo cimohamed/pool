de735a7e08d8878766aca3c1c8edaa72ce2e9464
Mohamed Chati <mohachat387@gmail.com>
2025-01-20 01:27:24 +0100

    print comb2

---
+++ comb3.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func comb3() {
+		for a := 0; a <= 98; a++ {
+			for b := a + 1; b <= 99; b++ {
+				z01.PrintRune(rune(a/10 + '0'))
+				z01.PrintRune(rune(a%10 + '0'))
+				z01.PrintRune(' ')
+				z01.PrintRune(rune(b/10 + '0'))
+				z01.PrintRune(rune(b%10 + '0'))
+				if !(a == 98 && b == 99) {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+		z01.PrintRune('\n')
+	}
+	func main(){
+		comb3()
+	}
+
+		
+	
