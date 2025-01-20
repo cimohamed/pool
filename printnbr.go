394c1da2f94963837719ff1d74d03de1c4866501
Mohamed Chati <mohachat387@gmail.com>
2025-01-20 21:45:01 +0100

    printnbr

---
+++ printnbr.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbr(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+	} else if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	arry := []int{}
+	for n > 0 {
+		a := n % 10
+		arry = append(arry, a)
+
+		n = n / 10
+	}
+	for i := len(arry) - 1; i >= 0; i-- {
+		z01.PrintRune(rune(arry[i] + 48))
+	}
+}
+
+func main() {
+	PrintNbr(-123)
+	PrintNbr(0)
+	PrintNbr(123)
+	z01.PrintRune('\n')
+}
