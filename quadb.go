4626627b26b3f1e8b1a6378c89caa80e53451cc9
Mohamed Chati <mohachat387@gmail.com>
2025-01-22 20:57:25 +0100

    quadB

--- quadb.go
+++ quadb.go
@@ -2,16 +2,17 @@ package main
 
 import "github.com/01-edu/z01"
 
-func QuadA(x, y int) {
+func QuadB(x, y int) {
 	for i := 1; i <= x; i++ {
 		for j := 1; j <= y; j++ {
-			if i == 1 && j == 1 || i == x && j == 1 || i == 1 && j == y || i == x && j == y {
-				z01.PrintRune('o')
+			if i == 1 && j == 1 || i == x && j == y {
+				z01.PrintRune('/')
+			} else if i == x && j == 1 || i == 1 && j == y {
+				z01.PrintRune('\\')
 			} else if i == 1 && j > 1 && j < y || i == x && j > 1 && j < y {
-				z01.PrintRune('-')
-
+				z01.PrintRune('*')
 			} else if j == 1 && i > 1 && i < x || j == y && i > 1 && i < x {
-				z01.PrintRune('|')
+				z01.PrintRune('*')
 			} else {
 				z01.PrintRune(' ')
 			}
@@ -20,11 +21,11 @@ func QuadA(x, y int) {
 	}
 }
 func main() {
-	QuadA(1,1)
+	QuadB(3, 5)
 }
+
 // $ go run .
 // o---o
 // |   |
 // o---o
 // $
-
