9ebb934941ec9d39ce068ce5c6dda77a923acde7
Mohamed Chati <mohachat387@gmail.com>
2025-02-24 18:28:36 +0000

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -7,11 +7,10 @@ import (
 func AlphaCount(s string) int {
 	var cont int
 	for i := 0; i < len(s)-1; i++ {
-		if (s[i]>='A'&& s[i]<='Z' || s[i]>='a' && s[i]<='z'){
+		if s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z' {
 			cont++
 		}
-		
-		
+
 	}
 	return cont
 }
