6254e11d04e291b617f34a63fc0861edf79ce497
Mohamed Chati <mohachat387@gmail.com>
2025-02-28 18:34:19 +0000

    strrev

--- strrev.go
+++ strrev.go
@@ -2,32 +2,16 @@ package main
 
 import (
 	"fmt"
-<<<<<<< HEAD
-=======
-	
->>>>>>> 39d85a9 (sortparams)
 )
 
 func StrRev(s string) string {
-	var r string
-<<<<<<< HEAD
-	tmp := []string{}
+	tmp := ""
 	for i := len(s) - 1; i >= 0; i-- {
-		tmp = append(tmp, string(s[i]))
+		tmp += string(s[i])
 	}
-	for j := 0; j <= len(tmp)-1; j++ {
-		r += tmp[j]
-	}
-	return r
-
-=======
-	tmp := []rune(s)
-	for j:=len(tmp)-1;j>=0;j--{
-		r+=string(tmp[j])
-	}
-return r
->>>>>>> 39d85a9 (sortparams)
+	return tmp
 }
+
 func main() {
 	s := "Hello World!"
 	s = StrRev(s)
